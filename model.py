630269b31d0a007bf2fac4a97b9c58149a2d9201
seoi hong <90823683+hongseoi@users.noreply.github.com>
2023-12-07 14:18:12 +0900

    fix folder and files

--- model.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    sample_text = "The Dow Jones Industrial Average (^DJI) turned green."
-
-    model = SentimentModel()
-    sentiment = model.predict(text=sample_text)
-    print(sentiment)
